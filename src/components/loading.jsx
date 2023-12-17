76e203e28053eb3cd33f56e284a85bc166566647
PunithG360 <punithg@figmatic.co>
2023-12-18 00:01:55 +0530

    fix: Speed analytics

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -16,6 +16,7 @@ export default function Loading() {
           animationData={defaultOptions.animationData}
           loop={defaultOptions.loop}
         />
+        Loading...
       </div>
     );
   }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,8 @@ import '../../styles/globals.css'
 
 import NewsContextProvider from '@/context/newsContext'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }) {
-  return <NewsContextProvider><Component {...pageProps} /><Analytics /></NewsContextProvider>
+  return <NewsContextProvider><Component {...pageProps} /><Analytics /><SpeedInsights/></NewsContextProvider>
 }
